8705c090092ebbcd4a3632d89e071e739313b35e
Fernando A. Maule <fer@maule.pro>
2023-01-08 00:16:57 +0100

    feat: deploy to docker swarm (#2)
    * feat: deployment
    
    * update pipeline
    
    * load secrets from docker swarm

--- src/components/config.js
+++ src/components/config.js
@@ -1,16 +1,35 @@
-const config = require('../../config.json');
+const { getSecrets } = require('docker-secret');
 
 // LOGGER
-const path = require('node:path')
-const { initLogger } = require('./logger'); 
-const componentName = path.parse(__filename).name
+const path = require('node:path');
+const { initLogger } = require('./logger');
+const componentName = path.parse(__filename).name;
 const { log } = initLogger(componentName);
 
 const initConfig = async () => {
-  log('∝ initializing component')
+  log('∝ initializing component');
+  
+  const secrets = getSecrets(process.env.SECRET_DIR);
+  const config = {
+    discord: {
+      token: secrets.BOT_DISCORD_TOKEN,
+      clientId: '1060895467933806642',
+      guildId: '762775384746885150',
+    },
+    tracker: {
+      token: secrets.BOT_TRACKER_TOKEN,
+    },
+    mongodb: {
+      uri: secrets.BOT_MONGO_URI,
+      dbName: 'jarry-dev',
+      usersCollection: 'users',
+      statsCollection: 'stats',
+    },
+  };
+
   return config;
-}
+};
 
 module.exports = {
-  initConfig
-}
+  initConfig,
+};
